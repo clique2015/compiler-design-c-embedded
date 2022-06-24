5c5070663b353acc60f6f41b2d30b7642a19a578
projectfpga <ezeuko.arinze@gmail.com>
2022-06-24 08:18:29 -0700

    compiler in c++

---
+++ test.cpp
@@ -0,0 +1,39 @@
+void testfunction(int sampleInteger){
+//    cout<<12;
+}
+int main(){
+//    string start = "hello world compiler";
+    int sampleInteger = 320;
+    int secondInteger =  300 + sampleInteger;
+    testfunction(sampleInteger);
+}
+
+
+int counter(int ac, int car){
+    int F;
+    int A;
+    int B;
+    int C;
+
+    if(F == 12){
+    //    F= 25*2+8/2-7*(A-9)+3*B-C;
+    A = 20;
+    }else 
+    if(C >= F){
+        F = 12;
+    }else 
+    if(C >= F){
+        F = 12;
+    }else 
+    if(C >= F){
+        F = 12;
+    }else 
+    if(C >= F){
+        F = 12;
+    }
+    else{
+         B = 12;
+    }
+}
+
+
